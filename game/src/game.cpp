aed5942f13ec43dd461e41917375d289704986af
Matthew Reed <mattreed1890@gmail.com>
2025-01-09 14:28:33 -0600

    remove player

--- game/src/game.cpp
+++ game/src/game.cpp
@@ -1,35 +1,9 @@
 #include "game.h"
-#include "player.h"
 #include "raylib.h"
-
-int map[] = {
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,    
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-};
 
 int main() {
     run();
     return 0;
-}
-
-void drawMap() {
-    int width = 12;
-    int height = 8;
-    for (int i = 0; i < width * height; i++) {
-        int tile = map[i];
-        int x = i % width;
-        int y = i / width;
-
-        if (tile > 0) {
-            DrawRectangle(x * 100.0f, y * 100.0f, 100.0f, 100.0f, RED);
-        }
-    }
 }
 
 void run() {
@@ -38,11 +12,8 @@ void run() {
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        updatePlayer();
         BeginDrawing();
-        ClearBackground(LIGHTGRAY);
-        drawMap();
-        drawPlayer();
+        ClearBackground(WHITE);
         EndDrawing();
     }
     CloseWindow();
